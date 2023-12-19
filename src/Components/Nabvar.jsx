46b46a861943e3a9b86becc492f60c544aebcd89
SARP9712 <alonsopaez1206@gmail.com>
2023-12-19 18:37:52 +0100

    rentcarSevilla

--- src/Components/Nabvar.jsx
+++ src/Components/Nabvar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Logo from '../assets/logo.png';
+import Logo from '../assets/Logo.png';
 import LenguajeSelector from './Shared/LenguajeSelector'
 import { FiMenu, FiShoppingCart, FiPhone, FiMail } from "react-icons/fi";
 import Carrito from './Shared/Carrito';
